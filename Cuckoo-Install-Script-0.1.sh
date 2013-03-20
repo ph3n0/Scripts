56681647884231325804fe9213e0cf21167ca719
Zac (ph3n0) <zac@phenotyne.com>
2013-03-19 20:34:19 -0500

    Checked for those semicolons
    Added in semicolons to try to make sure downloads were done.

--- Cuckoo-Install-Script-0.1.sh
+++ Cuckoo-Install-Script-0.1.sh
@@ -19,17 +19,14 @@ fi
 
 echo "Grabbing VirtualBox 4.2.10 and the Extension Pack \ "
 
-wget http://download.virtualbox.org/virtualbox/4.2.10/virtualbox-4.2_4.2.10-84104~Ubuntu~precise_amd64.deb
-
-dpkg -i virtualbox-4.2_4.2.10-84104~Ubuntu~precise_amd64.deb
-
-wget http://download.virtualbox.org/virtualbox/4.2.10/Oracle_VM_VirtualBox_Extension_Pack-4.2.10.vbox-extpack
-
-vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-4.2.10.vbox-extpack
+wget http://download.virtualbox.org/virtualbox/4.2.10/virtualbox-4.2_4.2.10-84104~Ubuntu~precise_amd64.deb;
+dpkg -i virtualbox-4.2_4.2.10-84104~Ubuntu~precise_amd64.deb;
+wget http://download.virtualbox.org/virtualbox/4.2.10/Oracle_VM_VirtualBox_Extension_Pack-4.2.10.vbox-extpack;
+vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-4.2.10.vbox-extpack;
 
 ## VirtualBox web interface
 cd /var/www/  
-wget https://phpvirtualbox.googlecode.com/files/phpvirtualbox-4.2-4.zip
+wget https://phpvirtualbox.googlecode.com/files/phpvirtualbox-4.2-4.zip;
 unzip phpvirtualbox-4.2-4.zip
 mv /var/www/phpvirtualbox-4.2-4 /var/www/phpvirtualbox  
 #chown -R CHANGME:CHANGME /var/www/phpvirtualbox/  
@@ -38,20 +35,20 @@ cp /var/www/phpvirtualbox/config.php-example /var/www/phpvirtualbox/config.php
 
 ## SSDEEP Python set
 cd /opt
-svn checkout http://pyssdeep.googlecode.com/svn/trunk/ pyssdeep  
+svn checkout http://pyssdeep.googlecode.com/svn/trunk/ pyssdeep;
 cd pyssdeep  
-python setup.py build  
-python setup.py install
+python setup.py build;
+python setup.py install;
 
 ## Yara and Python Support 
 cd /usr/src
-wget http://yara-project.googlecode.com/files/yara-1.6.tar.gz  
-tar -xvzf yara-1.6.tar.gz  
-cd yara-1.6  
-./configure  
-make  
-make check  
-make install   
+wget http://yara-project.googlecode.com/files/yara-1.6.tar.gz;
+tar -xvzf yara-1.6.tar.gz;
+cd yara-1.6
+./configure
+make;
+make check;
+make install;
 cd /usr/src
 wget http://yara-project.googlecode.com/files/yara-python-1.6.tar.gz  
 tar -xvzf yara-python-1.6.tar.gz  
